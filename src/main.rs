c8ac64355d847aa38d83e71bef1fd28690116781
phantekzy <mainilotfi@gmail.com>
2026-02-08 22:05:33 +0100

    added argument length validation and usage guide

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,9 @@ use std::env;
 fn main() {
     // Get commands from the CLI
     let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        println!("Usage: rwatch <seconds> <command>");
+        println!("Example: rwatch 2 \"netstat -ant\" ");
+        return;
+    }
 }
